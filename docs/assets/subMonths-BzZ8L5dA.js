07ddb08ac9fae74684c6898d4bb9b051f2bcdac5
Shafiq R. Khan <37314609+shfqrkhn@users.noreply.github.com>
2025-07-18 20:22:47 -0400

    Delete docs directory

--- docs/assets/subMonths-BzZ8L5dA.js
+++
@@ -1,1 +0,0 @@
-import{ax as t}from"./index-DwbuEFEH.js";function r(o,s){return t(o,-s)}export{r as s};
--- docs/assets/useAiFeatures-BStZ87Me.js
+++
@@ -1,1 +0,0 @@
-import{u as e,d as i}from"./index-DwbuEFEH.js";const t=()=>{const s=e(()=>i.settings.get("geminiApiKey"));return{isAiEnabled:!!(s!=null&&s.value),isLoading:s===void 0}};export{t as u};
