9b89c3131b778637f95a8a5bd73450a3be876c5c
Shafiq R. Khan <37314609+shfqrkhn@users.noreply.github.com>
2025-07-18 20:24:01 -0400

    Add files via upload

---
+++ docs/assets/subMonths-Du8FVnST.js
@@ -0,0 +1,1 @@
+import{ax as t}from"./index-Xj3-ztsO.js";function r(o,s){return t(o,-s)}export{r as s};
---
+++ docs/assets/useAiFeatures-BoFMrtPk.js
@@ -0,0 +1,1 @@
+import{u as e,d as i}from"./index-Xj3-ztsO.js";const t=()=>{const s=e(()=>i.settings.get("geminiApiKey"));return{isAiEnabled:!!(s!=null&&s.value),isLoading:s===void 0}};export{t as u};
